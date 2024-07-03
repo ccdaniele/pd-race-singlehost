95e2ea078a0beefda5d4afc13d22847d49e4a365
daniel calderon <daniel.calderon@datadoghq.com>
2024-07-03 17:23:25 -0400

    first

---
+++ pd-client-service/src/app/layout.js
@@ -0,0 +1,21 @@
+import './globals.css'
+import DatadogInit from '@/lib/datadog.init'
+import DatadogLogsInit from '@/lib/datadog.logs'
+
+
+export default function RootLayout({ children }) {
+
+
+
+  return (
+    <html>
+      <body>
+      <DatadogInit />
+      <DatadogLogsInit/> 
+        <main>              
+          {children}
+        </main> 
+      </body>
+    </html>
+  )
+}
